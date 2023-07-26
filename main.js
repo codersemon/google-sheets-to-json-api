422b945236f1f89f305c63f0ae9bd7631cd54927
Emon Khan <codersemon@gmail.com>
2023-07-26 19:51:39 +0600

    create google sheet to json api

---
+++ main.js
@@ -0,0 +1,25 @@
+function getSheetDataAsJSON() {
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+  var range = sheet.getDataRange();
+  var values = range.getValues();
+  var headers = values[0];
+  var jsonData = [];
+
+  for (var i = 1; i < values.length; i++) {
+    var row = values[i];
+    var rowObj = {};
+
+    for (var j = 0; j < headers.length; j++) {
+      rowObj[headers[j]] = row[j];
+    }
+
+    jsonData.push(rowObj);
+  }
+
+  return JSON.stringify(jsonData);
+}
+
+function doGet() {
+  return ContentService.createTextOutput(getSheetDataAsJSON())
+    .setMimeType(ContentService.MimeType.JSON);
+}
